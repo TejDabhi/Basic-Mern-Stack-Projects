09af6bb01b3499a40127ae1e1c5d193811210e12
Tej <157304155+TejDabhi@users.noreply.github.com>
2025-12-27 09:49:34 +0530

    Add files via upload

---
+++ Bcrypt/index.js
@@ -0,0 +1,57 @@
+const cors =require('cors')
+const express=require('express')
+const mongoose =require('mongoose')
+const bcrypt=require('bcrypt')
+
+const EmployeeModel=require('./models/Employee')
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/employee")
+
+app.post('/register',(req,res)=>{
+    const {name,email,password}=req.body
+    bcrypt.hash(password,10)
+    .then(hash=>{
+        EmployeeModel.create({name,email,password:hash})
+        .then((user)=>{
+            return res.json(user)
+        })
+        .catch(err=>{
+            return res.json(err)
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+app.post('/login',(req,res)=>{
+    const {email,password}=req.body
+    EmployeeModel.findOne({email})
+    .then((user)=>{
+        if(user){
+            bcrypt.compare(password,user.password,(err,response)=>{
+                if(err){
+                    return res.json({message:"The password is incorrect"})
+                }
+                if(response){
+                    return res.json({message:"The password is correct"})
+
+                }
+            })
+        }else{
+            return res.json({message:"No record Existed"})
+        }
+
+    })
+    .catch(err=>{
+        return res.json(err)
+    })
+})
+
+
+
+app.listen(3001,()=>{
+    console.log("Port 3001 running")
+})
